639a9cdc6d8899bcb3a95bb69ca1ef566306e623
Yasir Shariff <goodwonder5@gmail.com>
2022-08-18 10:14:09 +0300

    initial commit

---
+++ benches/bench.rs
@@ -0,0 +1,15 @@
+// #![feature(test)]
+
+// extern crate test;
+
+// use test::bench::{benchmark, black_box};
+
+// #[bench]
+// fn bench_add(b: &mut test::Bencher) {
+//     let mut storage = Storage::new();
+//     b.iter(|| {
+//         storage.add(black_box(1));
+//     });
+// }
+
+// TODO:: BENCHMARKING
